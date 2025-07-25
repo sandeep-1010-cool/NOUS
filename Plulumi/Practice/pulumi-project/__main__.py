93bf3a56c1f40eaa55b63ef40bb635efb576aab2
Vuyyala sandeep kumar <vuyyalasandeep1010@gmail.com>
2025-07-25 08:34:38 +0530

    Enhance installation guide for Claude AI by adding free API key options and alternatives, and update Pulumi project to create an EC2 instance with configuration support.

--- Plulumi/Practice/pulumi-project/__main__.py
+++ Plulumi/Practice/pulumi-project/__main__.py
@@ -1,10 +1,21 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
+import pulumi_aws as aws
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+config = pulumi.Config()
+instance_type = config.require("instanceType")
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Create an EC2 instance using the configuration value
+ami = aws.ec2.get_ami(
+    most_recent=True,
+    owners=["amazon"],
+    filters=[{"name": "name", "values": ["amzn2-ami-hvm-*"]}]
+)
+
+instance = aws.ec2.Instance(
+    "config-instance",
+    instance_type=instance_type,
+    ami=ami.id,
+    tags={"Name": "ConfigInstance"}
+)
+
+pulumi.export("instance_id", instance.id)
